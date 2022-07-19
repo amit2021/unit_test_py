10e3078794a26ae93eb5291e509416bdc8ec075f
Amit Kumar Singh <a.kumar2021singh@gmail.com>
2022-07-20 01:05:37 +0530

    third comiit

--- test_calc.py
+++ test_calc.py
@@ -5,7 +5,7 @@ import unittest
 class testCalc(unittest.TestCase):
 
     def test_add(self):
-        result=calc.add(10,5)
+        result=calc.add(9,6)
         self.assertEqual(result,15)
 
 if __name__== '__main__':
